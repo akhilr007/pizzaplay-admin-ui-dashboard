3f6cd3691165461718d964af4a929e09d44b9119
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-13 20:32:56 +0530

    add simple import

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+
 import { Categories } from "./pages/Categories";
+import HomePage from "./pages/HomePage";
 
 export const router = createBrowserRouter([
     {
