202febe7e826534cdc150c73e4defaa3f466f439
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-16 20:59:08 +0530

    add logout routes

--- src/http/api.ts
+++ src/http/api.ts
@@ -6,3 +6,5 @@ export const login = (credentials: Credentials) =>
     api.post("/auth/login", credentials);
 
 export const whoami = () => api.get("/auth/whoami");
+
+export const logout = () => api.post("/auth/logout");
