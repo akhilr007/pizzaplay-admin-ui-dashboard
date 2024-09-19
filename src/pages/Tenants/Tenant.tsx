2ae284c5a5c48ba6f7917a3ed7e7b2ef5dc85686
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-19 21:38:26 +0530

    add tenant table

--- src/pages/Tenants/Tenant.tsx
+++ src/pages/Tenants/Tenant.tsx
@@ -1,10 +1,46 @@
 import { RightOutlined } from "@ant-design/icons";
-import { Breadcrumb, Space } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Breadcrumb, Space, Table } from "antd";
 import { Link } from "react-router-dom";
 
+import { tenants } from "../../http/api";
 import { TenantFilter } from "./TenantFilter";
 
+const getTenants = async () => {
+    const { data } = await tenants();
+    const { data: tenantsData } = data;
+    return tenantsData;
+};
+
 export const Tenant = () => {
+    const {
+        data: tenants,
+        isLoading,
+        isError,
+        error
+    } = useQuery({
+        queryKey: ["users"],
+        queryFn: getTenants
+    });
+
+    const columns = [
+        {
+            title: "ID",
+            dataIndex: "id",
+            key: "id"
+        },
+        {
+            title: "Name",
+            dataIndex: "name",
+            key: "name"
+        },
+        {
+            title: "Address",
+            dataIndex: "address",
+            key: "address"
+        }
+    ];
+
     return (
         <Space direction="vertical" style={{ width: "100%" }} size="large">
             <Breadcrumb
@@ -14,12 +50,18 @@ export const Tenant = () => {
                     { title: "Tenants" }
                 ]}
             />
+
+            {isLoading && <div>Loading...</div>}
+            {isError && <div>{error.message}</div>}
+
             <TenantFilter
                 onFilterChange={() =>
                     (filterName: string, filterValue: string) => {
                         console.log(filterName, filterValue);
                     }}
             />
+
+            <Table columns={columns} dataSource={tenants} rowKey={"id"} />
         </Space>
     );
 };
