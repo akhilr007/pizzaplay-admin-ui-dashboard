6947e67968511d85f8e9cbc080c4c7249b1b435a
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-21 20:47:12 +0530

    add intended url redirect on refresh

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,13 +1,16 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 import { Sidebar } from "../components/Sidebar/Sidebar";
 import { useAuthStore } from "../store";
 
 export const Dashboard = () => {
+    const { pathname } = useLocation();
     const { user } = useAuthStore();
 
     if (user === null) {
-        return <Navigate to="/auth/login" replace={true} />;
+        return (
+            <Navigate to={`/auth/login?returnTo=${pathname}`} replace={true} />
+        );
     }
 
     const { role } = user;
