99893040cdec3a0418e7e026fb43ed4ade1c945e
Akhil Rajan <rajanakhil07@gmail.com>
2024-09-22 19:25:26 +0530

    remove id from tenants table

--- src/pages/Tenants/TenantsTable.tsx
+++ src/pages/Tenants/TenantsTable.tsx
@@ -21,11 +21,6 @@ export const TenantsTable: React.FC<Props> = ({
 }) => {
     const columns = [
         {
-            title: "ID",
-            dataIndex: "id",
-            key: "id"
-        },
-        {
             title: "Name",
             dataIndex: "name",
             key: "name"
